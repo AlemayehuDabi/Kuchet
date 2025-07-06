e2af93459f5e73d1a2c4d5260289a97c23a01414
Alemayehu <alemayehudabi606@gmail.com>
2025-07-06 15:27:46 +0300

    FEAT: Link the product image in the url

--- components/shared/products/product-card.tsx
+++ components/shared/products/product-card.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 interface ProductListProps {
   product: SampleDataType;
@@ -14,19 +15,21 @@ export function ProductCard({ product }: ProductListProps) {
   return (
     <Card className="dark:bg-white">
       <CardHeader>
-        <Image
-          src={product.images[0]} // Displaying the first image
-          alt={product.name}
-          width={500}
-          height={300}
-          className="object-contain mb-4 rounded h-[15rem] w-[20rem]"
-          priority={true}
-        />
+        <Link href={`${product.slug}`}>
+          <Image
+            src={product.images[0]} // Displaying the first image
+            alt={product.name}
+            width={500}
+            height={300}
+            className="object-contain mb-4 rounded h-[15rem] w-[20rem]"
+            priority={true}
+          />
+        </Link>
       </CardHeader>
       <CardContent>
         <div className="flex flex-col gap-2 dark:text-black">
           <h2 className="text-xl font-semibold">{product.name}</h2>
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <p className="text-gray-600 text-lg ">
               ${product.price.toFixed(2)}
             </p>
